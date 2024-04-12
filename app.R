a34e339f200e0bcd1007d4adad5e68835c8cfca7
limnologylauren <lauren.magliozzi@colorado.edu>
2024-04-12 10:30:57 -0600

    Remove Library Install

--- app.R
+++ app.R
@@ -1,8 +1,8 @@
 # This is the list of packages that are needed. Checks to see if they are 
 #  installed. If any are missing, this installs them.
-list.of.packages <- c("shiny","tidyverse","readxl","bslib")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
+#list.of.packages <- c("shiny","tidyverse","readxl","bslib")
+#new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+#if(length(new.packages)) install.packages(new.packages)
 library(shiny)
 source("ui.R")
 source("server.R")
